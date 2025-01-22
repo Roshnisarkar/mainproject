9147a8e4985a55e522bc58db2531d53ab5b4900c
Roshnisarkar <roshnisarkardev20@gmail.com>
2025-01-22 21:30:45 +0530

    update

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function red(){
+    let a= document.querySelector("#change")
+    a.style.backgroundColor="crimson"
+}
+function green(){
+    let a= document.querySelector("#change")
+    a.style.backgroundColor="green"
+}
+function blue(){
+    let a= document.querySelector("#change")
+    a.style.backgroundColor="blue"
+   
+}
+
+
+ 
