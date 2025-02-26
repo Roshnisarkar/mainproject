95ea1dc139740c443d3807f9756816b3c75abfa0
Roshnisarkar <roshnisarkardev20@gmail.com>
2025-02-26 23:35:43 +0530

    js

--- javascript.js
+++ javascript.js
@@ -13,4 +13,61 @@ function blue(){
 }
 
 
+// let signup =()=>{
+//     let nam=document.querySelector("#firstname").value
+//     let last=document.querySelector("#lastname").value
+//     let email=document.querySelector("#email-inp").value
+//     let pass=document.querySelector("#pass-inp").value
+//     let err=document.querySelector("#er")
+//     if(nam==""){
+//         err.innerHTML="invalid"
+//         return false
+//     }
+// else if( last=="" ){
+//     err.innerHTML="invalid"
+//     return false
+// }
+
+// else if( email=="" ){
+//     err.innerHTML="invalid"
+//     return false
+// }
+// else if( pass=="" ){
+//     err.innerHTML="invalid"
+//     return false
+// }
+
+
+// localStorage.setItem("name",nam)
+// localStorage.setItem("lastname",last)
+// localStorage.setItem("email",email)
+// localStorage.setItem("password",pass)
+
+// location.href="homepagge.html"
+
+// return false
+
+// }
+
+// let login =()=>{
+//     let n =document.querySelector("#name").value
+//     let l=document.querySelector("#lname").value
+//     let e=document.querySelector("#semail").value
+//     let p=document.querySelector("#spass").value
+//     // let er=document.querySelector("#err")
+
+// let lfname=localStorage.getItem("name")
+// let lsname=localStorage.getItem("lastname")
+// let lsemail=localStorage.getItem("email")
+// let lspass=localStorage.getItem("password")
+
+// if(n==lfname && l==lsname && e==lsemail && p==lspass)
+//     location.href="homepagge.html"
+
+// return false
+
+// }
+
+
+
  
